9c8c580f92e940a3a4410ebb22efe1eec2b534a8
itayhau <itayhau@gmail.com>
2021-07-06 19:40:56 +0300

    Create readline1.js

---
+++ readline1.js
@@ -0,0 +1,13 @@
+console.log('hello world!');
+var x = 1;
+console.log(`x = ${x+1}`)
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
+  readline.question('Who are you?', name => {
+    console.log(`Hey there ${name}!`);
+    readline.close();
+  });
